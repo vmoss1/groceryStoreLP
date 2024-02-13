8ba0cf3c41f08d64a716fd72045991e38e5b76d4
Veronica <vivettemoss@yahoo.com>
2024-02-13 17:17:21 -0600

    phase2complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react";
+import configureStore from "./store/store";
+import { populateProduce } from "./store/produce";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// import { populateProduce } from "./store/produce";
+// ...
+
+const store = configureStore();
+
+// if (import.meta.env.MODE !== "production") {
+//   window.store = store;
+//   window.populateProduce = populateProduce;
+// }
+
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+  window.populateProduce = populateProduce;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
-
